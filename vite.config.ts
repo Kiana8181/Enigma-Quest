12593b05211acb7e5050343b61038ffef01a1965
Kiana8181 <kianahashemi8@gmail.com>
2024-07-29 00:29:38 +0330

    change path

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Enigma-Quest/",
   plugins: [react()],
-  server: {
-    host: "0.0.0.0", // This allows external connections
-    port: 5173, // Your desired port number
-  },
 });
